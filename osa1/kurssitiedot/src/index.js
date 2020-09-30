5d307164a1ac55fff86e6cb085c1529922a89658
jcbehm <yompatchi@gmail.com>
2020-09-30 19:44:35 +0300

    Tehtava 1.5 valmis.

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -47,33 +47,35 @@ const Total = (props) => {
 {/* The App */ }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
 
       {/* Header */}
-      <Header course={course} />
+      <Header course={course.name} />
 
       {/* Content */}
-      <Content parts={parts} />
+      <Content parts={course.parts} />
 
       {/* Total */}
-      <Total parts={parts} />
+      <Total parts={course.parts} />
 
     </div>
   )
