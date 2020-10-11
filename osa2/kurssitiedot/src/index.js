c105a8fd420136d592159d90fcc851ab88bdcac8
jcbehm <yompatchi@gmail.com>
2020-10-11 22:08:29 +0300

    Tehtava 2.4 valmis.

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,91 +1,130 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Course = ({ course }) => {
+const Curriculum = ({ courses }) => {
 
-  const Header = (props) => {
-    return (
-      <>
-        <h1>{props.course}</h1>
-      </>
-    )
-  }
+  const Course = ({ course }) => {
 
-  const Content = (props) => {
-
-    const Part = (props) => {
+    const Header = ( {course} ) => {
       return (
         <>
-          <p>
-            {props.part} {props.exercises}
-          </p>
+          <h2>{course.name}</h2>
+        </>
+      )
+    }
+
+    const Content = ( {course} ) => {
+
+      const Part = ( {part} ) => {
+        return (
+          <>
+            <p>
+              {part.name} {part.exercises}
+            </p>
+          </>
+        )
+      }
+
+      return (
+        <>
+          {course.parts.map(part =>
+            <Part key={part.exercises} part={part} />
+          )}
+        </>
+      )
+    }
+
+    const Total = ( {course} ) => {
+      let tehtavat = []
+      course.parts.forEach(part => {
+        tehtavat = tehtavat.concat(part.exercises)
+      }
+      )
+      const reducer =
+        (accumulator, currentValue) => accumulator + currentValue
+      const total = tehtavat.reduce(reducer)
+
+      return (
+        <>
+          <b>total of {total} exercises</b>
         </>
       )
     }
 
     return (
       <>
-        {props.parts.map(part =>
-          <Part key={part.exercises} part={part.name} exercises={part.exercises} />
-        )}
+        < Header course={course} />
+
+        < Content course={course} />
+
+        < Total course={course} />
+
       </>
     )
   }
 
-  const Total = (props) => {
-    let tehtavat = []
-    props.parts.forEach(part => {
-      tehtavat = tehtavat.concat(part.exercises)}
-    )
-    const reducer =
-      (accumulator, currentValue) => accumulator + currentValue
-    const total = tehtavat.reduce(reducer)
-
-    return (
-      <>
-        <b>total of {total} exercises</b>
-      </>
-    )
-  }
 
   return (
     <>
-      < Header course={course.name} />
-
-      < Content parts={course.parts} />
-
-      < Total parts={course.parts} />
-
+      <h1>Web development curriculum</h1>
+      {courses.map(course => 
+        <Course key={course.id} course={course} />
+      )}
     </>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'Redux',
-        exercises: 11
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <>
-      <Course course={course} />
+      {
+      <Curriculum courses={courses} />
+      }
     </>
   )
 }
