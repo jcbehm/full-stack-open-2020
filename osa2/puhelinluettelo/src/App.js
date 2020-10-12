9c8244285d53d4343a37decf3925751a23872302
jcbehm <yompatchi@gmail.com>
2020-10-12 23:07:22 +0300

    Tehtava 2.9 valmis.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,11 +3,15 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244'}
-  ]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [showingFilter, setShowingFilter] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -41,9 +45,29 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setShowingFilter(event.target.value)
+    if (event.target.value === '') {
+      setShowAll(true)
+    } else {
+      setShowAll(false)
+    }
+    
+  }
+
+  const personsToShow = showAll
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(showingFilter.toLowerCase()))
+
   return (
     <>
       <h2>Phonebook</h2>
+      filter shown with
+      <input
+        value={showingFilter}
+        onChange={handleFilterChange}
+      />
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:
@@ -65,7 +89,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => 
+        {personsToShow.map((person) =>
           <Person key={person.name} person={person} />
         )}
       </div>
