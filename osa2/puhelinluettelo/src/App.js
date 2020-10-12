3177fa4ad458e657f2d04847faf9b31650e3a800
jcbehm <yompatchi@gmail.com>
2020-10-12 21:43:47 +0300

    Tehtava 2.6 valmis.

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import Name from './components/Name'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(noteObject))
+    setNewName('')
+  }
+
+  const handleFormChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleFormChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => 
+          <Name key={person.name} name={person.name} />
+        )}
+      </div>
+    </>
+  )
+
+}
+
+export default App
---
+++ osa2/puhelinluettelo/src/components/Name.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Name = ({ name }) => {
+  return (
+    <p>{name}</p>
+  )
+}
+
+export default Name
