0c10b75e8a247b5f0876a13578c289aa37682eb5
jcbehm <yompatchi@gmail.com>
2020-10-16 19:44:54 +0300

    Tehtava 2.12 valmis.

---
+++ osa2/maiden-tiedot/src/App.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Language = ({ language }) => (
+  <li>
+    {language}
+  </li>
+)
+
+const CountryInfo = ({ country }) => (
+  <div>
+    <h2>{country.name}</h2>
+    <p>
+      capital {country.capital} <br />
+      population {country.population}
+    </p>
+    <h3>languages</h3>
+    {country.languages.map((language, i) =>
+      <Language key={i} language={language.name} />
+    )}
+    <br />
+    <img
+      src={country.flag}
+      alt="flag"
+      width="100"
+    />
+  </div>
+)
+
+const Line = ({ line }) => (
+  <div>
+    {line}
+  </div>
+)
+
+const Display = ({ countriesToShow }) => {
+  let info = []
+  info = countriesToShow.length > 10
+    ? ['Too many matches, specify another filter']
+    : countriesToShow.length < 2
+      ? [' ']
+      : countriesToShow.map(country => country.name);
+
+  if (countriesToShow.length === 1) {
+    return (
+      <>
+        <CountryInfo country={countriesToShow[0]} />
+      </>
+    )
+  }
+
+  return (
+    <div>
+      {info.map((line, i) =>
+        <Line key={i} line={line} />
+      )}
+    </div>
+  )
+}
+
+function App() {
+  const [newSearch, setNewSearch] = useState('')
+  const [allCountries, setAllCountries] = useState([])
+  let countriesToShow = []
+
+  if (newSearch !== '')
+  countriesToShow = allCountries.filter(country =>
+    country.name.toLowerCase().includes(newSearch.toLowerCase())
+  )
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setAllCountries(response.data)
+      })
+  }, [])
+
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+  }
+
+
+  return (
+    <div>
+      find countries
+      <input
+        value={newSearch}
+        onChange={handleSearchChange}
+      />
+      <Display countriesToShow={countriesToShow} />
+    </div>
+  )
+}
+
+export default App;
