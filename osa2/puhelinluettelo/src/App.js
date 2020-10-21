a874c82b34a9acff190ed94fdbe58a11c0abc315
jcbehm <yompatchi@gmail.com>
2020-10-21 18:54:07 +0300

    Tehtava 2.15 valmis.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,18 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
-const [persons, setPersons] = useState([])
-  /*
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  */
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showingFilter, setShowingFilter] = useState('')
@@ -20,16 +12,15 @@ const [persons, setPersons] = useState([])
 
 
   useEffect(() => {
-      console.log('effect')
-      axios
-        .get('http://localhost:3001/persons')
-        .then(response => {
-          console.log('promise fulfilled')
-          setPersons(response.data)
-        })
-    }, [])
-  
-    console.log('render', persons.length, 'persons')
+    console.log('effect')
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+
+  console.log('render', persons.length, 'persons')
 
 
 
--- osa2/puhelinluettelo/src/components/PersonForm.js
+++ osa2/puhelinluettelo/src/components/PersonForm.js
@@ -1,59 +1,64 @@
 import React from 'react'
+import personService from '../services/persons'
 
 const PersonForm = (props) => {
-    const addPerson = (event) => {
-      event.preventDefault()
-      const noteObject = {
-        name: props.newName,
-        number: props.newNumber
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: props.newName,
+      number: props.newNumber
+    }
+
+    let includes = false
+    props.persons.forEach(person => {
+      if (person.name === props.newName) {
+        includes = true
       }
-  
-      let includes = false
-      props.persons.forEach(person => {
-        if (person.name === props.newName) {
-          includes = true
-        }
-      })
-  
-      if (includes) {
-        window.alert(`${props.newName} is already added to phonebook`)
-      } else {
-        props.setPersons(props.persons.concat(noteObject))
-      }
-  
-      props.setNewName('')
-      props.setNewNumber('')
+    })
+
+    if (includes) {
+      window.alert(`${props.newName} is already added to phonebook`)
+    } else {
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          props.setPersons(props.persons.concat(returnedPerson))
+        })
     }
-  
-    const handleNameChange = (event) => {
-      props.setNewName(event.target.value)
-    }
-  
-    const handleNumberChange = (event) => {
-      props.setNewNumber(event.target.value)
-    }
-  
-    return (
-      <form onSubmit={addPerson}>
-          <div>
-            name:
-            <input
-              value={props.newName}
-              onChange={handleNameChange}
-            />
-          </div>
-          <div>
-            number:
-              <input
-              value={props.newNumber}
-              onChange={handleNumberChange}
-            />
-          </div>
-          <div>
-            <button type="submit">add</button>
-          </div>
-        </form>
-    )
+
+    props.setNewName('')
+    props.setNewNumber('')
   }
 
-  export default PersonForm
+  const handleNameChange = (event) => {
+    props.setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    props.setNewNumber(event.target.value)
+  }
+
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name:
+            <input
+          value={props.newName}
+          onChange={handleNameChange}
+        />
+      </div>
+      <div>
+        number:
+              <input
+          value={props.newNumber}
+          onChange={handleNumberChange}
+        />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+export default PersonForm
