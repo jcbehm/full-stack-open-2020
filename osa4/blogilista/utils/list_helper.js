bcc699a23b59de9bd44951c063c0bac1e94e17c4
jcbehm <yompatchi@gmail.com>
2020-11-13 16:24:19 +0200

    Tehtava 4.5 valmis.

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -10,6 +10,21 @@ const totalLikes = (blogs) => {
   return likes
 }
 
+const favoriteBlog = (blogs) => {
+  let favorite = { title: '', author: '', likes: -1 }
+  blogs.forEach(blog => {
+    if (favorite.likes < blog.likes) {
+      favorite = blog
+    }
+  })
+  const returningFavorite = {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes
+  }
+  return returningFavorite
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
