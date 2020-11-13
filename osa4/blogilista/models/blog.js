962edde48a8e421fa9de53f9cdaa3b4475589f5b
jcbehm <yompatchi@gmail.com>
2020-11-13 14:01:06 +0200

    Tehtava 4.2 valmis.

---
+++ osa4/blogilista/models/blog.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  url: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  likes: {
+    type: Number,
+    required: true
+  },
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
