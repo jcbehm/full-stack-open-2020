0ae9d199b607886ee110fed927c411a411992172
jcbehm <yompatchi@gmail.com>
2020-11-17 19:18:33 +0200

    Tehtava 4.17 valmis.

--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -27,9 +27,6 @@ blogSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    if (!returnedObject.likes) {
-      returnedObject.likes = 0
-    }
   }
 })
 
