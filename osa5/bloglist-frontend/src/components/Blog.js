d664a3b2ee98cab8daac9652cd658b6f136dedde
jcbehm <yompatchi@gmail.com>
2020-11-24 18:40:12 +0200

    Tehtava 5.7 valmis.

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,44 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (!view) {
+    return (
+      <div style={blogStyle} onClick={() => setView(true)}>
+        {blog.title}
+        {" "}
+        <button onClick={() => setView(true)}>view</button>
+      </div>
+      )
+  }
+
+  return (
+  <div style={blogStyle}>
+    {blog.title}
+    {" "}
+    <button onClick={() => setView(false)}>hide</button>
+    <br/>
+
+    {blog.url}
+    <br/>
+    
+    likes {blog.likes}
+    {" "}
+    <button>like</button>
+    <br/>
+
+    {blog.author}
   </div>
-)
+  )
+}
 
 export default Blog
